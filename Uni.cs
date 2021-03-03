3d179d0963d70b6dc21d51bccd1cdac75efe4106
Teo <66961474+TeofilPopov@users.noreply.github.com>
2021-03-03 13:49:12 +0100

    Uni

---
+++ Uni.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uni
+{
+    public class Uni
+    {
+        private List<Student> _students;
+        private int _currentId;
+
+        public Uni()
+        {
+            _students = new List<Student>();
+            _currentId = 10000;
+        }
+
+        public void AddStudent(string name)
+        {
+            if (_students.Any(x=>x.Name.Equals(name)))
+            {
+                throw new ArgumentException($"Student with name {name} alreade exists.");
+            }
+
+            Student student = new Student(name, _currentId++);
+            _students.Add(student);
+        }
+    }
+}
